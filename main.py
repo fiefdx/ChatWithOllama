3d118320f4f7046bd21fc632599362c74f28db9e
fiefdx <fiefdx@gmail.com>
2025-09-06 17:17:29 -0600

    input text box support editing

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ import numpy
 import whisper
 from ollama import Client
 from TTS.api import TTS
+import emoji
 
 __version__ = "0.0.1"
 
@@ -48,7 +49,17 @@ theme_data = {
             "border_width": "1",
             "open_button_width": "10"
         }
-    }
+    },
+    "text_box": {
+        "colours": {
+            "normal_text": "#ffffff"
+        }
+    },
+    "text_entry_box": {
+        "colours": {
+            "normal_text": "#ffffff"
+        }
+    },
 }
 theme_file_path = "theme.json"
 with open(theme_file_path, "w") as fp:
@@ -68,7 +79,7 @@ class AudioPlayer(Process):
         try:
             signal.signal(signal.SIGTERM, self.sig_handler)
             signal.signal(signal.SIGINT, self.sig_handler)
-            self.tts = TTS("tts_models/en/ljspeech/glow-tts", progress_bar = False).to("cuda")
+            self.tts = TTS("tts_models/en/ljspeech/glow-tts", progress_bar = False).to("cpu") # ("cuda")
             while True:
                 try:
                     task = None
@@ -95,8 +106,7 @@ class AudioPlayer(Process):
                         else:
                             if os.path.exists("./output.wav"):
                                 os.remove("./output.wav")
-                            # task = task.replace("`", " ")
-                            # task = task.replace('"', " ")
+                            task = emoji.replace_emoji(task, "")
                             self.tts.tts_to_file(text = task, file_path = "./output.wav")
                             samplerate, data = read("./output.wav")
                             self.stream = sd.play(data, samplerate)
@@ -126,9 +136,9 @@ class ThinkThread(StoppableThread):
         Thread.__init__(self)
         self.main_thread = None
         self.query = None
-        self.model = whisper.load_model("base.en") # base.en, small.en
+        self.model = whisper.load_model("base.en", device = 'cpu') # base.en, small.en
         self.client = Client("http://localhost:11434")
-        self.chat_model = 'gemma3:4b'
+        self.chat_model = 'llama3.2:3b'
         self.context = []
         self.context_length = 4096
 
@@ -138,13 +148,14 @@ class ThinkThread(StoppableThread):
                 if not self.stopped():
                     try:
                         if self.query is not None:
-                            r = self.model.transcribe("input.wav")
-                            self.main_thread.message = r["text"].strip()
-                            self.main_thread.update_query = True
+                            if self.query == "input.wav":
+                                r = self.model.transcribe("input.wav")
+                                self.main_thread.message = r["text"].strip()
+                                self.main_thread.update_query = True
                             self.context.append({"role": "user", "content": self.main_thread.message})
                             if len(self.context) > self.context_length:
                                 self.context.pop(0)
-                            r = self.client.chat(model = self.chat_model, messages = self.context)
+                            r = self.client.chat(model = self.chat_model, messages = self.context, think = False)
                             self.query = None
                             self.main_thread.response = r
                             self.main_thread.update_reply = True
@@ -196,14 +207,16 @@ class UserInterface(object):
                 self.models.append(model["model"])
         self.models.sort()
         self.manager = pygame_gui.UIManager((1280, 640), theme_path = "theme.json")
-        self.query_box = pygame_gui.elements.ui_text_box.UITextBox("", relative_rect = pygame.Rect(10, 10, 1100, 70), manager = self.manager)
+        self.query_box = pygame_gui.elements.ui_text_entry_box.UITextEntryBox(relative_rect = pygame.Rect(10, 10, 1100, 70), manager = self.manager)
         self.update_query = False
         self.reply_box = pygame_gui.elements.ui_text_box.UITextBox("", relative_rect = pygame.Rect(10, 90, 1260, 500), manager = self.manager)
         self.update_reply = False
         self.stop_button = pygame_gui.elements.UIButton(relative_rect = pygame.Rect((10, 595), (100, 40)), text = 'Stop', manager = self.manager)
         self.play_button = pygame_gui.elements.UIButton(relative_rect = pygame.Rect((120, 595), (100, 40)), text = 'Play', manager = self.manager)
         self.discard_button = pygame_gui.elements.UIButton(relative_rect = pygame.Rect((230, 595), (100, 40)), text = 'Discard', manager = self.manager)
-        self.chat_models = pygame_gui.elements.ui_drop_down_menu.UIDropDownMenu(options_list = self.models, starting_option = self.think_thread.chat_model, relative_rect = pygame.Rect((340, 595), (300, 40)), expansion_height_limit = 300, manager = self.manager)
+        self.send_button = pygame_gui.elements.UIButton(relative_rect = pygame.Rect((340, 595), (100, 40)), text = 'Send', manager = self.manager)
+        self.chat_models = pygame_gui.elements.ui_drop_down_menu.UIDropDownMenu(options_list = self.models, starting_option = self.think_thread.chat_model, relative_rect = pygame.Rect((450, 595), (300, 40)), expansion_height_limit = 300, manager = self.manager)
+        self.record_button = pygame_gui.elements.UIButton(relative_rect = pygame.Rect((760, 595), (100, 40)), text = 'Record', manager = self.manager)
         self.new_chat_button = pygame_gui.elements.UIButton(relative_rect = pygame.Rect((1170, 595), (100, 40)), text = 'New Chat', manager = self.manager)
 
     def quit(self):
@@ -236,6 +249,12 @@ class UserInterface(object):
         print("change model to: %s" % model)
         self.think_thread.chat_model = model
 
+    def change_message(self, text):
+        self.message = text
+
+    def send(self):
+        self.think_thread.query = "new_message"
+
     def process_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -244,17 +263,6 @@ class UserInterface(object):
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.quit()
-                elif event.key == pygame.K_SPACE:
-                    if os.path.exists("input.wav"):
-                        os.remove("input.wav")
-                    self.sf = sf.SoundFile("input.wav", mode = 'x', samplerate = self.samplerate, channels = 2)
-                    self.sd = sd.InputStream(samplerate = self.samplerate, channels = 2, callback = callback)
-                    self.sd.start()
-                    self.status = "recording"
-                    self.message = None
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE:
-                    self.status = "waiting"
             elif event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == self.play_button:
                     self.play()
@@ -264,9 +272,25 @@ class UserInterface(object):
                     self.discard()
                 elif event.ui_element == self.new_chat_button:
                     self.new()
+                elif event.ui_element == self.send_button:
+                    self.send()
+                elif event.ui_element == self.record_button:
+                    self.status = "waiting"
+            elif event.type == pygame_gui.UI_BUTTON_START_PRESS:
+                if event.ui_element == self.record_button:
+                    if os.path.exists("input.wav"):
+                        os.remove("input.wav")
+                    self.sf = sf.SoundFile("input.wav", mode = 'x', samplerate = self.samplerate, channels = 2)
+                    self.sd = sd.InputStream(samplerate = self.samplerate, channels = 2, callback = callback)
+                    self.sd.start()
+                    self.status = "recording"
+                    self.message = None
             elif event.type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
                 if event.ui_element == self.chat_models:
                     self.change_chat_model(event.text)
+            elif event.type == pygame_gui.UI_TEXT_ENTRY_CHANGED:
+                if event.ui_element == self.query_box:
+                    self.change_message(event.text)
             self.manager.process_events(event)
 
     def render(self):
